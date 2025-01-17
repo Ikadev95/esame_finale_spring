c330ff9ea4dbf285f48fbcc932799dc0842619dc
Ikadev95 <fegughi@gmail.com>
2025-01-17 10:01:09 +0100

    Initial commit

---
+++ src/main/java/com/epicode/esame_finale_spring/EsameFinaleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.esame_finale_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameFinaleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameFinaleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/esame_finale_spring/EsameFinaleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.esame_finale_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameFinaleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
